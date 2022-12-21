f82632a5b6f67ac09d5ba859c332ffd3a0549509
Anton <designedbyart1808@gmail.com>
2022-12-21 22:40:16 +0200

    fix

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -5,7 +5,7 @@ import storage from 'redux-persist/lib/storage';
 const persistConfig = {
   key: 'contactsList',
   storage,
-  whiteList: ['list'],
+  whiteList: ['contactsList'],
 };
 
 const contactsSlice = createSlice({
